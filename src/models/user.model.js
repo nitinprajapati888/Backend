779b0db70854868ce750bef771ce235c09da83de
Nitin Prajapati <nitinprajapati88811@gmail.com>
2023-12-03 23:39:04 +0530

    model first commit

---
+++ src/models/user.model.js
@@ -0,0 +1,51 @@
+import mongoose, {Schema} from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            lowercase: true,
+            unique: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            lowercase: true,
+            unique: true,
+            trim: true,
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        coverImeage: {
+            type: String, // cloudinary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'password is required']
+        },
+        refreshTokens: {
+            type: String
+        }
+    },{
+        timestamps: true
+    }
+)
+
+export const User = mongoose.model("User", userSchema)
